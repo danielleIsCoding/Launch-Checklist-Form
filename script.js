b5f1abf389092b698c6610ce5caf756c34c13e6d
danielleiscoding <danmck2@yahoo.com>
2021-03-19 01:08:20 -0500

    everything but the json part working

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
+  
    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
      
+      
       if (pilotName.value === ""  || copilotName.value === "" || 
             fuelLevel.value === ""|| cargoMass.value === "" ) {
          alert("All fields are required!");
@@ -14,8 +18,7 @@ window.addEventListener("load", function() {
          event.preventDefault();
       }
 
-      if (!isNaN(pilotName.value) || 
-      !isNaN(copilotName.value)) {
+      if (!isNaN(pilotName.value) || !isNaN(copilotName.value)) {
          alert("Name is an Invalid Entry!");
          // stop the form submission
          event.preventDefault();
@@ -26,9 +29,54 @@ window.addEventListener("load", function() {
          //stop the form submission
          event.preventDefault();
       }
+         
+         const faultyItems = document.getElementById("faultyItems");
+         const launchStatus = document.getElementById("launchStatus");
+         const fuelStatus = document.getElementById("fuelStatus");
+         const cargoStatus = document.getElementById("cargoStatus");
+      
+        
+         
+      if (fuelLevel.value < 10000 || cargoMass.value > 10000 ){
+         
+            faultyItems.style.visibility = 'visible';
+            launchStatus.innerHTML = "Shuttle not ready for launch";
+            launchStatus.style.color = "Red";
+         
+         
+         if (fuelLevel.value < 10000){
+              
+            fuelStatus.innerHTML = "Fuel level to low to launch"
+         }
 
-   });
+         if(cargoMass.value > 10000){
+              
+            cargoStatus.innerHTML = "Cargo mass too high to launch"   
+         }
+      
+      }else{
+         launchStatus.innerHTML = "Shuttle ready for launch";
+         launchStatus.style.color = "Green";
+      }
+
+      //Changes pilot name
+      const pilotStatus = document.getElementById("pilotStatus");
+      const copilotStatus = document.getElementById("copilotStatus");
+      pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch.`;
+      copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready for launch.`;
+   
+   })
+   
+     
+      
 });
+   
+  
+
+      
+
+
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
